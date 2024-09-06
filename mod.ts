22b02d5a1e00e0f807fb7c3729c1e6554ed04c0b
Mattias Runge-Broberg <mattias@runge.se>
2024-09-06 07:35:16 +0200

    Fix exports

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-export type * from '@ein/bash-parser';
+export * from '@ein/bash-parser';
 export * from './src/context.ts';
 export * from './src/executor.ts';
-export type * from './src/types.ts';
+export * from './src/types.ts';
